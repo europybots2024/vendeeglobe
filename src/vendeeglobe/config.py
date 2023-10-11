1e5f686e2a7eb5e95032267d3e333611da73cda5
Neil Vaytet <neil.vaytet@esss.se>
2023-10-11 09:33:13 +0200

    make config frozen dataclass

--- src/vendeeglobe/config.py
+++ src/vendeeglobe/config.py
@@ -1,26 +1,26 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-
+from dataclasses import dataclass
 from pathlib import Path
+from typing import List
 
 from .core import Checkpoint
 
 
+@dataclass(frozen=True)
 class Config:
-    def __init__(self):
-        self.map_file = "world.jpg"
-        self.map_radius = 6371
-        self.resourcedir = Path(__file__).parent / "resources"
-        self.ntracers = 5000
-        self.tracer_lifetime = 50
-        self.bot_polling_interval = 1.0
-        self.start = Checkpoint(longitude=-4.773949, latitude=48.333422, radius=5.0)
-        self.checkpoints = [
-            Checkpoint(latitude=2.806318, longitude=-168.943864, radius=2000.0),
-            Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1200.0),
-        ]
-        self.scores = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
-        self.player_update_interval = 1.0
-        self.graphics_update_interval = 0.1
-        self.time_update_interval = 1.0
-        self.forecast_length = 5.0  # in days
+    map_file: str = "world.jpg"
+    map_radius: float = 6371.0
+    resourcedir: Path = Path(__file__).parent / "resources"
+    ntracers: int = 5000
+    tracer_lifetime: int = 50
+    start: Checkpoint = Checkpoint(longitude=-4.773949, latitude=48.333422, radius=5.0)
+    checkpoints: List[Checkpoint] = [
+        Checkpoint(latitude=2.806318, longitude=-168.943864, radius=2000.0),
+        Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1200.0),
+    ]
+    scores: list = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
+    player_update_interval: float = 0.1  # 1.0
+    graphics_update_interval: float = 0.1
+    time_update_interval: float = 1.0
+    forecast_length: int = 5  # in days

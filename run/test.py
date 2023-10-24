8812f4f622329cf6a2cf0b40a54a8a2e71c883b5
Neil Vaytet <neil.vaytet@ess.eu>
2023-10-24 21:21:46 +0200

    update time limit

--- run/test.py
+++ run/test.py
@@ -6,6 +6,7 @@ from template_bot import Bot, Bot2
 
 players = {"Alice": Bot, "Bob": Bot2}
 
+
 start = None
 # start = vg.Location(longitude=-68.004373, latitude=18.180470)
 
@@ -14,4 +15,4 @@ for team, bot in players.items():
     bots.append(bot())
     bots[-1].team = team
 
-vg.play(bots=bots, start=start, seed=None, time_limit=60 * 5)
+vg.play(bots=bots, start=start, seed=None, time_limit=60 * 8)

5104fc6c15edb9ce31480136dc62a2fbd151b49a
Neil Vaytet <neil.vaytet@ess.eu>
2023-11-09 20:11:20 +0100

    remove template bot and make template repo

--- run/test.py
+++ run/test.py
@@ -1,25 +1,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import vendeeglobe as vg
-from template_bot import Bot, Bot2
-
-
-players = {"Alice": Bot, "Bob": Bot2}
+from template_bot import Bot
 
 
 start = None
 # start = vg.Location(longitude=-68.004373, latitude=18.180470)
 
-bots = []
-for team, bot in players.items():
-    bots.append(bot())
-    bots[-1].team = team
-
 vg.play(
-    bots=bots,
-    start=start,
-    seed=None,
-    time_limit=60 * 8,
-    speedup=None,
-    course_preview=None,
+    bots=[Bot()],  # List of bots to use
+    start=start,  # Starting location for all bots
+    seed=None,  # Seed for generating the weather
+    time_limit=60 * 8,  # Time limit in seconds
+    speedup=None,  # Time speedup factor
+    course_preview=None,  # A list of checkpoints should be supplied
 )

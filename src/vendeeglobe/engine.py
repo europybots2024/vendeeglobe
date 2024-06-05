d1d219d60ee7e6125cc84dab0831de208f365a37
Neil Vaytet <neil.vaytet@ess.eu>
2024-06-05 23:55:41 +0200

    new scores

--- src/vendeeglobe/engine.py
+++ src/vendeeglobe/engine.py
@@ -278,7 +278,7 @@ class Engine:
         for i, (name, score) in enumerate(sorted_scores.items()):
             self.score_boxes[i].setText(
                 f'<div style="color:{self.players[name].color}">&#9632;</div> '
-                f"{i+1}. {name[:config.max_name_length]}: {score}"
+                f"{i+1}. {name[:config.max_name_length]}: {score:.2f}"
             )
 
         sorted_times = dict(sorted(fastest_times.items(), key=lambda item: item[1]))

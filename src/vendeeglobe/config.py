1cd304085fdfc24f2f6894158978ff404b039fa1
Neil Vaytet <neil.vaytet@ess.eu>
2024-07-06 22:43:31 +0200

    remove version pinning and fix frozen dataclass error in py > 3.10

--- src/vendeeglobe/config.py
+++ src/vendeeglobe/config.py
@@ -9,27 +9,41 @@ from .core import Checkpoint
 
 @dataclass(frozen=True)
 class Config:
-    map_file: str = "world.jpg"
-    map_radius: float = 6371.0
-    resourcedir: Path = Path(__file__).parent / "resources"
-    ntracers: int = 5000
-    tracer_lifetime: int = 50
-    start: Checkpoint = Checkpoint(longitude=-1.81, latitude=46.494275, radius=5.0)
-    checkpoints: Tuple[Checkpoint, Checkpoint] = (
+    map_file: str
+    map_radius: float
+    resourcedir: Path
+    ntracers: int
+    tracer_lifetime: int
+    start: Checkpoint
+    checkpoints: Tuple[Checkpoint, Checkpoint]
+    graphics_update_interval: float
+    time_update_interval: float
+    forecast_length: int
+    weather_update_interval: float
+    seconds_to_hours: float
+    score_step: float
+    max_name_length: int
+
+
+config = Config(
+    map_file="world.jpg",
+    map_radius=6371.0,
+    resourcedir=Path(__file__).parent / "resources",
+    ntracers=5000,
+    tracer_lifetime=50,
+    start=Checkpoint(longitude=-1.81, latitude=46.494275, radius=5.0),
+    checkpoints=(
         Checkpoint(latitude=2.806318, longitude=-168.943864, radius=2000.0),
         Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1200.0),
-    )
-    graphics_update_interval: float = 0.1
-    time_update_interval: float = 1.0
-    forecast_length: int = 5  # in days
-    weather_update_interval: float = 3.0  # 3s = 12 hours
-    seconds_to_hours = 4.0
-    avatar_size = [64, 64]
-    score_step = 100_000
-    max_name_length = 15
-
-
-config = Config()
+    ),
+    graphics_update_interval=0.1,
+    time_update_interval=1.0,
+    forecast_length=5,  # in days
+    weather_update_interval=3.0,  # 3s = 12 hours
+    seconds_to_hours=4.0,
+    score_step=100_000,
+    max_name_length=15,
+)
 
 
 # Timing:
